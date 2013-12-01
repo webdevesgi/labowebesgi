74f508850e462ac06a865f8ebd968fe7ea4d0e76
Rémy Hannequin <remy.hannequin@gmail.com>
2013-12-01 02:21:18 +0100

    Display talks on user page

--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @user, :name, :email, :created_at, :updated_at
+json.extract! @user, :name, :email, :talks, :created_at, :updated_at
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,22 +2,27 @@ require 'spec_helper'
 
 describe UsersController do
 
-  before(:each) do
-    @user = FactoryGirl.create(:user)
-    sign_in @user
-  end
+  login_user
+
+  let(:user) { FactoryGirl.create(:user) }
 
   describe "GET 'show'" do
 
     it "should be successful" do
-      get :show, id: @user.id
+      get :show, id: user.id
       response.should be_success
     end
 
     it "should find the right user" do
-      get :show, id: @user.id
-      assigns(:user).should == @user
+      get :show, id: user.id
+      assigns(:user).should == user
     end
+
+    # it "should display the list of talks" do
+    #   talk = FactoryGirl.create(:talk, speaker: user)
+    #   visit user_path(user)
+    #   page.should have_content(talk.title)
+    # end
 
   end
 
