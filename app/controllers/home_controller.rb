de0c7c31ee247ed1853c7df075009785401de871
Rémy Hannequin <remy.hannequin@gmail.com>
2013-12-02 01:40:48 +0100

    Improve app in many ways

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.first(3)
+    @articles = Article.order('created_at DESC').first(3)
+    @next_event = Event.next
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,6 +7,28 @@ describe HomeController do
       get 'index'
       response.should be_success
     end
+
+    describe "Check articles" do
+      before(:each) do
+        @articles = FactoryGirl.create_list(:article, 10)
+      end
+
+      it "should assign articles" do
+        get 'index'
+        assigns(:articles).should == @articles.last(3).reverse
+      end
+    end
+
+    describe "Check next event" do
+      before(:each) do
+        @next_event = FactoryGirl.create(:future_event)
+      end
+
+      it "should assign next_event" do
+        get 'index'
+        assigns(:next_event).should == @next_event
+      end
+    end
   end
 
 end
