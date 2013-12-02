0e60b5c6c8ffe4b99dd53d8a7f905eda07a5e31c
Rémy Hannequin <remy.hannequin@gmail.com>
2013-12-02 23:32:09 +0100

    Fix: wtf did I just commit

--- db/migrate/20131130222652_modify_user_id_from_articles.rb
+++ db/migrate/20131130222652_modify_user_id_from_articles.rb
@@ -1,5 +1,5 @@
 class ModifyUserIdFromArticles < ActiveRecord::Migration
   def change
-    change_column :articles, :user_id, 'integer USING CAST(column_to_change AS integer)'
+    change_column :articles, :user_id, 'integer USING CAST(user_id AS integer)'
   end
 end
