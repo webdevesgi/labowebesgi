32106e3bbd475ae4b6355d6081991d1bff4256fd
Rémy Hannequin <remy.hannequin@gmail.com>
2014-03-13 16:50:07 +0100

    Typo fix for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'email_spec'
 require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
-  config.include(EmailSpec::Helpers)
-  config.include(EmailSpec::Matchers)
-
-  # Devise config
-  config.include Devise::TestHelpers, type: :controller
-  config.extend ControllerMacros, type: :controller
+  config.include EmailSpec::Helpers
+  config.include EmailSpec::Matchers
 
   # ## Mock Framework
   #
@@ -42,7 +38,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = 'random'
 
 
   # Database Cleaner
@@ -55,7 +51,7 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each, :js => true) do
+  config.before(:each, js: true) do
     DatabaseCleaner.strategy = :truncation
   end
 
--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,3 +1,6 @@
+require_relative 'controller_macros'
+
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
+  config.extend ControllerMacros, type: :controller
 end
