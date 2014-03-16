26ed27f064d109914d9e0ebd546721c4c99cdd2f
Rémy Hannequin <remy.hannequin@gmail.com>
2014-03-17 00:49:58 +0100

    Enforce locale validation and remove useless migration about Article casting foreign key

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,8 @@ module Labowebesgi
       password: ENV['GMAIL_PASSWORD']
     }
 
+    config.i18n.enforce_available_locales = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
